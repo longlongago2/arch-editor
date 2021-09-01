3283c4dfd73685913be32b035efe4a4d79dfbe12
longlongago2 <1789136583@qq.com>
2021-09-01 17:23:14 +0800

    fix bugs

--- src/components/AtomicTable.js
+++ src/components/AtomicTable.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState, useRef } from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
